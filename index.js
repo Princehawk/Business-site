55fe1b6d0f4de66d726247848fa3a2867945b78f
Wilhelm <princehawk9984@gmail.com>
2025-10-26 13:38:18 -0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 {
-    // navigation code
-     var sidemenu = document.getElementById("sidemenu");
+  // navigation code
+  var sidemenu = document.getElementById('sidemenu')
 
-      function openmenu() {
-        sidemenu.style.right = "0";
-      }
+  function openmenu () {
+    sidemenu.style.right = '0'
+  }
 
-      function closemenu() {
-        sidemenu.style.right = "-200px";
-      }
+  function closemenu () {
+    sidemenu.style.right = '-200px'
+  }
 
-      
+  fetch('http://localhost:8080/api/users')
+    .then(res => res.json())
+    .then(data => {
+      console.log('Users:', data)
+    })
+    .catch(err => {
+      console.error(err)
+    })
 }
