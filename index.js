f43fc0751a4c99f0cb1f679a0f5824041ae1bf39
Wilhelm <princehawk9984@gmail.com>
2025-10-26 10:51:23 -0700

    business site vs.1

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+    // navigation code
+     var sidemenu = document.getElementById("sidemenu");
+
+      function openmenu() {
+        sidemenu.style.right = "0";
+      }
+
+      function closemenu() {
+        sidemenu.style.right = "-200px";
+      }
+
+      
+}
